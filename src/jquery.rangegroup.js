a8f0dbf11f9522ad533fa29b700174b1adf50e62
Jonas Hovgaard <j@jhovgaard.dk>
2015-08-30 13:01:26 +0200

    Added support for elements with no ID

--- src/jquery.rangegroup.js
+++ src/jquery.rangegroup.js
@@ -4,23 +4,22 @@ $(document).on('input', 'input[range-group]', function() {
   selectedRange = $(this);
   var group = selectedRange.attr('range-group');
   var max = selectedRange.attr('range-group-max-sum');
-  var total = 0;
 
   var ranges = $('input[range-group=' + group + ']');
-
+  var total = 0;
   ranges.each(function(i, item) {
-    console.log(item, $(item).val())
     total += parseInt($(item).val());
   });
 
   var left = max - total;
 
   if(left < 0) {
+
     var half = Math.ceil(left / 2);
 
     ranges.each(function(i, item) {
       $item = $(item);
-      if ($item.attr('id') == selectedRange.attr('id')) {
+      if ($item.get(0) === selectedRange.get(0)) {
         return true;
       };
       var newValue = parseInt($item.val()) + half;
